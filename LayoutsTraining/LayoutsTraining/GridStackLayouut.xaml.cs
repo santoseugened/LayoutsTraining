cec3523e94d98b28c04855e3e406c9152e12e384
Eugene Santos <eugene.santos@service101plus.com>
2017-10-05 16:19:53 +0800

    Add project files.

---
+++ LayoutsTraining/LayoutsTraining/GridStackLayouut.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LayoutsTraining
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class GridStackLayouut : ContentPage
+    {
+        public GridStackLayouut()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_OnClicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new GridLayout());
+        }
+    }
+}
